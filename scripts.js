63881b62a13723111a40a027b38062c70f00530a
Robert <69278716+banyekalaok@users.noreply.github.com>
2024-07-28 20:19:17 +0800

    Update scripts.js GPT Improve

--- scripts.js
+++ scripts.js
@@ -42,109 +42,135 @@ var countyData = {
   }
 };
 
-var currentIndex = 0;
+// Populate the dropdown menu with county names
+var dropdown = d3.select("#county-dropdown");
+for (var county in countyData) {
+  dropdown.append("option")
+          .attr("value", county)
+          .text(county);
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-  var mapContainer = document.getElementById("map-container");
-  var chartContainer = document.getElementById("chart-container");
-  var countyChartsContainer = document.getElementById("county-charts-container");
-  var dropdownContainer = document.querySelector(".dropdown-container");
-  var countyDropdown = document.getElementById("county-dropdown");
-  var countyPopulationChart = document.getElementById("county-population-chart");
-  var countyEmploymentChart = document.getElementById("county-employment-chart");
-  var countyIncomeChart = document.getElementById("county-income-chart");
-
-  var countyDropdownOptions = Object.keys(countyData).map(function(county) {
-    return `<option value="${county}">${county}</option>`;
-  }).join('');
-  countyDropdown.innerHTML += countyDropdownOptions;
-
-  document.getElementById("prev-button").addEventListener("click", function() {
-    if (currentIndex > 0) {
-      currentIndex--;
-      showPage(currentIndex);
-    }
-  });
-
-  document.getElementById("next-button").addEventListener("click", function() {
-    if (currentIndex < 5) {
-      currentIndex++;
-      showPage(currentIndex);
-    }
-  });
-
-  countyDropdown.addEventListener("change", function() {
-    var selectedCounty = countyDropdown.value;
-    if (selectedCounty) {
-      updateChart(countyPopulationChart, countyData[selectedCounty].population, "Population");
-      updateChart(countyEmploymentChart, countyData[selectedCounty].employment, "Employment");
-      updateChart(countyIncomeChart, countyData[selectedCounty].income, "Income");
-    }
-  });
-
-  function showPage(index) {
-    mapContainer.classList.toggle("active", index === 0 || index === 4);
-    chartContainer.classList.toggle("active", index === 1 || index === 2 || index === 3);
-    countyChartsContainer.classList.toggle("active", index === 5);
-    dropdownContainer.classList.toggle("hidden", index !== 4 && index !== 5);
-    switch(index) {
-      case 0:
-        document.querySelector("#map").className = "states";
-        break;
-      case 4:
-        document.querySelector("#map").className = "counties";
-        break;
-      case 5:
-        // Clear previous chart content
-        countyPopulationChart.innerHTML = '';
-        countyEmploymentChart.innerHTML = '';
-        countyIncomeChart.innerHTML = '';
-        countyDropdown.value = '';
-        break;
-    }
+dropdown.on("change", function() {
+  var selectedCounty = this.value;
+  if (selectedCounty) {
+    updateCountyCharts(selectedCounty);
   }
-
-  function updateChart(container, data, label) {
-    container.innerHTML = ''; // Clear previous chart content
-
-    var svg = d3.select(container)
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 300);
-
-    var x = d3.scaleBand()
-      .domain(d3.range(data.length))
-      .range([0, 500])
-      .padding(0.1);
-
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data)])
-      .nice()
-      .range([300, 0]);
-
-    svg.append("g")
-      .attr("transform", "translate(0,300)")
-      .call(d3.axisBottom(x).tickFormat(function(d, i) { return 2010 + i; }));
-
-    svg.append("g")
-      .call(d3.axisLeft(y));
-
-    svg.selectAll(".bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d, i) { return x(i); })
-      .attr("y", function(d) { return y(d); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return 300 - y(d); });
-
-    svg.append("text")
-      .attr("x", 250)
-      .attr("y", -10)
-      .attr("text-anchor", "middle")
-      .text(label + " (2010-2022)");
-  }
-
-  showPage(currentIndex); // Initial page display
 });
+
+// Update charts for the selected county
+function updateCountyCharts(countyName) {
+  var selectedData = countyData[countyName];
+  updateChart(selectedData.population, countyName + " Population", [100000, 400000], "county-population-chart");
+  updateChart(selectedData.employment, countyName + " Employment", [100000, 300000], "county-employment-chart");
+  updateChart(selectedData.income, countyName + " Income", [50000, 70000], "county-income-chart");
+}
+
+// Update existing functions to handle multiple charts
+function updateChart(data, yAxisText, yRange, chartId) {
+  var chartSvg = d3.select("#" + chartId + " svg");
+  var margin = {top: 20, right: 30, bottom: 30, left: 40};
+  var chartWidth = 800 - margin.left - margin.right;
+  var chartHeight = 400 - margin.top - margin.bottom;
+  var years = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"];
+
+  if (chartSvg.empty()) {
+    chartSvg = d3.select("#" + chartId)
+                 .append("svg")
+                 .attr("width", chartWidth + margin.left + margin.right)
+                 .attr("height", chartHeight + margin.top + margin.bottom)
+               .append("g")
+                 .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    chartSvg.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", `translate(0,${chartHeight})`);
+
+    chartSvg.append("g")
+            .attr("class", "y-axis");
+
+    chartSvg.append("text")
+            .attr("class", "axis-label")
+            .attr("x", chartWidth / 2)
+            .attr("y", chartHeight + margin.bottom - 10)
+            .attr("fill", "black")
+            .attr("text-anchor", "middle")
+            .text("Year");
+  }
+
+  var x = d3.scaleBand()
+            .domain(years)
+            .range([0, chartWidth])
+            .padding(0.1);
+
+  var y = d3.scaleLinear()
+            .range([chartHeight, 0])
+            .domain(yRange);
+
+  var xAxis = chartSvg.select(".x-axis");
+  var yAxis = chartSvg.select(".y-axis");
+
+  xAxis.call(d3.axisBottom(x).tickSizeOuter(0));
+
+  yAxis.transition()
+       .duration(750)
+       .call(d3.axisLeft(y))
+       .select(".axis-label")
+       .text(yAxisText);
+
+  var bars = chartSvg.selectAll(".bar")
+                     .data(data);
+
+  bars.enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", (d, i) => x(years[i]))
+      .attr("width", x.bandwidth())
+      .attr("y", chartHeight)
+      .attr("height", 0)
+      .merge(bars)
+      .transition()
+      .duration(750)
+      .attr("y", d => y(d))
+      .attr("height", d => chartHeight - y(d));
+
+  bars.exit().remove();
+}
+
+// Button functionality to switch between visualizations
+var pages = ["map", "population", "employment", "income", "counties", "new-page"];
+var currentPage = 0;
+
+function showPage(index) {
+  d3.select("#map-container").classed("active", index === 0 || index === 4 || index === 5);
+  d3.select("#chart-container").classed("active", index === 1 || index === 2 || index === 3);
+  d3.select("#county-charts-container").classed("active", index === 4);
+  d3.select(".dropdown-container").classed("active", index === 4);
+
+  if (index === 0) {
+    d3.select("#map").attr("class", "states");
+  } else if (index === 4) {
+    d3.select("#map").attr("class", "counties");
+  } else {
+    d3.select("#map").attr("class", "");
+  }
+
+  if (index === 1) {
+    updateChart(populationData, "Population", [3500000, 3700000], "chart");
+  } else if (index === 2) {
+    updateChart(employmentData, "Employment", [2100000, 2400000], "chart");
+  } else if (index === 3) {
+    updateChart(incomeData, "Per Capita Income", [60000, 85000], "chart");
+  }
+}
+
+// Pagination functionality
+d3.select("#next-button").on("click", function() {
+  currentPage = (currentPage + 1) % pages.length;
+  showPage(currentPage);
+});
+
+d3.select("#prev-button").on("click", function() {
+  currentPage = (currentPage - 1 + pages.length) % pages.length;
+  showPage(currentPage);
+});
+
+showPage(currentPage); // Show the initial page
