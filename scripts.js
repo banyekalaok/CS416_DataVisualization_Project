676a9c8b8ae199fad807fb972133881084e02d92
Robert <69278716+banyekalaok@users.noreply.github.com>
2024-07-28 17:12:14 +0800

    Update scripts.js Add csv connections

--- scripts.js
+++ scripts.js
@@ -1,33 +1,44 @@
-// Dummy data for counties (for dropdown functionality)
-var countyData = {
-  "Fairfield": [300000, 305000, 310000, 315000, 320000, 325000, 330000, 335000, 340000, 345000, 350000, 355000, 360000],
-  "Hartford": [250000, 255000, 260000, 265000, 270000, 275000, 280000, 285000, 290000, 295000, 300000, 305000, 310000],
-  "Litchfield": [200000, 205000, 210000, 215000, 220000, 225000, 230000, 235000, 240000, 245000, 250000, 255000, 260000],
-  "Middlesex": [150000, 155000, 160000, 165000, 170000, 175000, 180000, 185000, 190000, 195000, 200000, 205000, 210000],
-  "New Haven": [350000, 355000, 360000, 365000, 370000, 375000, 380000, 385000, 390000, 395000, 400000, 405000, 410000],
-  "New London": [175000, 180000, 185000, 190000, 195000, 200000, 205000, 210000, 215000, 220000, 225000, 230000, 235000],
-  "Tolland": [125000, 130000, 135000, 140000, 145000, 150000, 155000, 160000, 165000, 170000, 175000, 180000, 185000],
-  "Windham": [100000, 105000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000, 155000, 160000]
-};
+// Dummy data for counties will be updated with actual data from the CSV file
+var countyData = {};
 
-// Populate the dropdown menu with county names
-var dropdown = d3.select("#county-dropdown");
-for (var county in countyData) {
-  dropdown.append("option")
-          .attr("value", county)
-          .text(county);
-}
+// Read data from CSV and populate the countyData object
+d3.csv("ct_emp_inc.csv").then(function(data) {
+  data.forEach(function(d) {
+    var county = d.county;
+    if (!countyData[county]) {
+      countyData[county] = {
+        population: [],
+        employment: [],
+        income: []
+      };
+    }
+    countyData[county].population.push(+d.population);
+    countyData[county].employment.push(+d.employment);
+    countyData[county].income.push(+d.income);
+  });
 
-dropdown.on("change", function() {
-  var selectedCounty = this.value;
-  if (selectedCounty) {
-    updateCountyChart(selectedCounty);
+  // Populate the dropdown menu with county names
+  var dropdown = d3.select("#county-dropdown");
+  for (var county in countyData) {
+    dropdown.append("option")
+            .attr("value", county)
+            .text(county);
   }
+
+  dropdown.on("change", function() {
+    var selectedCounty = this.value;
+    if (selectedCounty) {
+      updateCountyCharts(selectedCounty);
+    }
+  });
 });
 
-function updateCountyChart(countyName) {
+// Function to update all three charts for the selected county
+function updateCountyCharts(countyName) {
   var selectedData = countyData[countyName];
-  updateChart(selectedData, countyName + " Population", [100000, 400000]);
+  updateChart(selectedData.population, countyName + " Population", [d3.min(selectedData.population) - 10000, d3.max(selectedData.population) + 10000], "population-chart");
+  updateChart(selectedData.employment, countyName + " Employment", [d3.min(selectedData.employment) - 10000, d3.max(selectedData.employment) + 10000], "employment-chart");
+  updateChart(selectedData.income, countyName + " Income", [d3.min(selectedData.income) - 5000, d3.max(selectedData.income) + 5000], "income-chart");
 }
 
 // Connecticut Population, Employment, and Per Capita Income Bar Charts
@@ -35,9 +46,6 @@ var chartWidth = 1163;
 var chartHeight = 720;
 var margin = {top: 30, right: 20, bottom: 50, left: 50};
 
-var populationData = [3580279, 3594193, 3605259, 3610314, 3614695, 3611995, 3607688, 3607615, 3611318, 3607159, 3597362, 3623355, 3626205];
-var employmentData = [2171940, 2206253, 2223243, 2246505, 2266061, 2288144, 2301331, 2300273, 2314465, 2297910, 2217432, 2289551, 2391946];
-var incomeData = [61392, 62964, 63555, 61999, 64482, 66220, 67550, 69146, 72157, 74173, 77383, 80691, 82938];
 var years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
 
 var chartSvg = d3.select("#chart")
@@ -79,7 +87,7 @@ yAxis.append("text")
      .text(yAxisLabel);
 
 var bars = chartSvg.selectAll(".bar")
-                   .data(populationData)
+                   .data([])
                    .enter().append("rect")
                    .attr("class", "bar")
                    .attr("x", (d, i) => x(years[i]))
@@ -87,21 +95,23 @@ var bars = chartSvg.selectAll(".bar")
                    .attr("width", x.bandwidth())
                    .attr("height", d => chartHeight - margin.bottom - y(d));
 
-function updateChart(data, yAxisText, yRange) {
+function updateChart(data, yAxisText, yRange, chartId) {
   y.domain(yRange);
 
-  yAxis.transition()
-       .duration(750)
-       .call(d3.axisLeft(y));
+  d3.select("#" + chartId + " .y-axis")
+    .transition()
+    .duration(750)
+    .call(d3.axisLeft(y));
 
-  yAxis.select(".axis-label")
-       .text(yAxisText);
+  d3.select("#" + chartId + " .y-axis .axis-label")
+    .text(yAxisText);
 
-  bars.data(data)
-      .transition()
-      .duration(750)
-      .attr("y", d => y(d))
-      .attr("height", d => chartHeight - margin.bottom - y(d));
+  d3.selectAll("#" + chartId + " .bar")
+    .data(data)
+    .transition()
+    .duration(750)
+    .attr("y", d => y(d))
+    .attr("height", d => chartHeight - margin.bottom - y(d));
 }
 
 // Button functionality to switch between visualizations
@@ -110,7 +120,7 @@ var currentPage = 0;
 
 function showPage(index) {
   d3.select("#map-container").classed("active", index === 0 || index === 4 || index === 5);
-  d3.select("#chart-container").classed("active", index === 1 || index === 2 || index === 3);
+  d3.select("#chart-container").classed("active", index === 1 || index === 2 || index === 3 || index === 5);
   d3.select(".dropdown-container").classed("active", index === 5);
 
   // Update class for map image
@@ -123,11 +133,11 @@ function showPage(index) {
   }
 
   if (index === 1) {
-    updateChart(populationData, "Population", [3500000, 3700000]);
+    updateChart(populationData, "Population", [3500000, 3700000], "chart");
   } else if (index === 2) {
-    updateChart(employmentData, "Employment", [2100000, 2400000]);
+    updateChart(employmentData, "Employment", [2100000, 2400000], "chart");
   } else if (index === 3) {
-    updateChart(incomeData, "Per Capita Income", [60000, 85000]);
+    updateChart(incomeData, "Per Capita Income", [60000, 85000], "chart");
   }
 }
 
