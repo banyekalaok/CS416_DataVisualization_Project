6c070ee904fec7e39cc303957807254cd0d20108
Robert <69278716+banyekalaok@users.noreply.github.com>
2024-07-28 23:58:38 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,68 +60,90 @@ dropdown.on("change", function() {
 // Update charts for the selected county
 function updateCountyCharts(countyName) {
   var selectedData = countyData[countyName];
-  updateChart(d3.select("#county-population-svg"), selectedData.population, countyName + " Population", [100000, 400000]);
-  updateChart(d3.select("#county-employment-svg"), selectedData.employment, countyName + " Employment", [100000, 300000]);
-  updateChart(d3.select("#county-income-svg"), selectedData.income, countyName + " Income", [50000, 70000]);
+  updateChart(selectedData.population, countyName + " Population", [100000, 400000]);
+  updateChart(selectedData.employment, countyName + " Employment", [100000, 300000]);
+  updateChart(selectedData.income, countyName + " Income", [50000, 70000]);
 }
 
-// Function to update a bar chart
-function updateChart(svg, data, yAxisText, yRange) {
-  var width = 800, height = 400, margin = {top: 30, right: 20, bottom: 50, left: 50};
-  
-  var x = d3.scaleBand()
-            .domain(d3.range(data.length))
-            .range([margin.left, width - margin.right])
-            .padding(0.1);
+// Update existing functions to handle multiple charts
+// Connecticut Population, Employment, and Per Capita Income Bar Charts
+var chartWidth = 800;
+var chartHeight = 400;
+var margin = {top: 30, right: 20, bottom: 50, left: 50};
 
-  var y = d3.scaleLinear()
-            .range([height - margin.bottom, margin.top])
-            .domain(yRange);
+var populationData = [3580279, 3594193, 3605259, 3610314, 3614695, 3611995, 3607688, 3607615, 3611318, 3607159, 3597362, 3623355, 3626205];
+var employmentData = [2171940, 2206253, 2223243, 2246505, 2266061, 2288144, 2301331, 2300273, 2314465, 2297910, 2217432, 2289551, 2391946];
+var incomeData = [61392, 62964, 63555, 61999, 64482, 66220, 67550, 69146, 72157, 74173, 77383, 80691, 82938];
+var years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
 
-  var yAxis = d3.axisLeft(y);
+var chartSvg = d3.select("#chart")
+                 .attr("width", chartWidth)
+                 .attr("height", chartHeight);
 
-  svg.attr("width", width)
-     .attr("height", height);
+var x = d3.scaleBand()
+          .domain(years)
+          .range([margin.left, chartWidth - margin.right])
+          .padding(0.1);
 
-  svg.selectAll("*").remove();  // Clear existing content
+var y = d3.scaleLinear()
+          .range([chartHeight - margin.bottom, margin.top]);
 
-  svg.append("g")
-     .attr("class", "x-axis")
-     .attr("transform", `translate(0,${height - margin.bottom})`)
-     .call(d3.axisBottom(x).tickFormat(i => 2010 + i))
-     .append("text")
-     .attr("class", "axis-label")
-     .attr("x", width / 2)
-     .attr("y", margin.bottom - 10)
-     .attr("fill", "black")
-     .attr("text-anchor", "middle")
-     .text("Year");
+var yAxisLabel = "Population";
 
-  svg.append("g")
-     .attr("class", "y-axis")
-     .attr("transform", `translate(${margin.left},0)`)
-     .call(yAxis)
-     .append("text")
+chartSvg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", translate(0,${chartHeight - margin.bottom}))
+        .call(d3.axisBottom(x).tickSizeOuter(0))
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("x", chartWidth / 2)
+        .attr("y", margin.bottom - 10)
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
+        .text("Year");
+
+var yAxis = chartSvg.append("g")
+                    .attr("class", "y-axis")
+                    .attr("transform", translate(${margin.left},0));
+
+yAxis.append("text")
      .attr("class", "axis-label")
      .attr("x", -margin.left)
      .attr("y", margin.top - 10)
      .attr("fill", "black")
      .attr("text-anchor", "start")
-     .text(yAxisText);
+     .text(yAxisLabel);
 
-  svg.selectAll(".bar")
-     .data(data)
-     .enter().append("rect")
-     .attr("class", "bar")
-     .attr("x", (d, i) => x(i))
-     .attr("y", d => y(d))
-     .attr("width", x.bandwidth())
-     .attr("height", d => height - margin.bottom - y(d));
+var bars = chartSvg.selectAll(".bar")
+                   .data(populationData)
+                   .enter().append("rect")
+                   .attr("class", "bar")
+                   .attr("x", (d, i) => x(years[i]))
+                   .attr("y", d => y(d))
+                   .attr("width", x.bandwidth())
+                   .attr("height", d => chartHeight - margin.bottom - y(d));
+
+function updateChart(data, yAxisText, yRange) {
+  y.domain(yRange);
+
+  yAxis.transition()
+       .duration(750)
+       .call(d3.axisLeft(y));
+
+  yAxis.select(".axis-label")
+       .text(yAxisText);
+
+  bars.data(data)
+      .transition()
+      .duration(750)
+      .attr("y", d => y(d))
+      .attr("height", d => chartHeight - margin.bottom - y(d));
 }
 
-// Page handling logic
-var pageIndex = 0;
-var pageCount = 6;
+
+// Button functionality to switch between visualizations
+var pages = ["map", "population", "employment", "income", "counties", "new-page"];
+var currentPage = 0;
 
 function showPage(index) {
   // Clear the 'active' class from all containers
@@ -136,13 +158,13 @@ function showPage(index) {
     d3.select("#map").attr("class", "states");
   } else if (index === 1) {
     d3.select("#chart-container").classed("active", true);
-    updateChart(d3.select("#population-chart-svg"), populationData, "Population", [3500000, 3700000]);
+    updateChart(populationData, "Population", [3500000, 3700000]);
   } else if (index === 2) {
     d3.select("#chart-container").classed("active", true);
-    updateChart(d3.select("#employment-chart-svg"), employmentData, "Employment", [2100000, 2400000]);
+    updateChart(employmentData, "Employment", [2100000, 2400000]);
   } else if (index === 3) {
     d3.select("#chart-container").classed("active", true);
-    updateChart(d3.select("#income-chart-svg"), incomeData, "Per Capita Income", [60000, 85000]);
+    updateChart(incomeData, "Per Capita Income", [60000, 85000]);
   } else if (index === 4) {
     d3.select("#map-container").classed("active", true);
     d3.select("#map").attr("class", "counties");
@@ -151,19 +173,20 @@ function showPage(index) {
     d3.select("#map").attr("class", "counties");
     d3.select("#county-charts-container").classed("active", true);
     d3.select(".dropdown-container").classed("active", true);
-    var selectedCounty = dropdown.property("value");
-    updateCountyCharts(selectedCounty);
+    updateCountyCharts(county);
   }
 }
 
-d3.select("#prev-button").on("click", function() {
-  pageIndex = (pageIndex - 1 + pageCount) % pageCount;
-  showPage(pageIndex);
-});
 
+// Pagination functionality
 d3.select("#next-button").on("click", function() {
-  pageIndex = (pageIndex + 1) % pageCount;
-  showPage(pageIndex);
+  currentPage = (currentPage + 1) % pages.length;
+  showPage(currentPage);
 });
 
-showPage(pageIndex);
+d3.select("#prev-button").on("click", function() {
+  currentPage = (currentPage - 1 + pages.length) % pages.length;
+  showPage(currentPage);
+});
+
+showPage(currentPage); // Show the initial page
